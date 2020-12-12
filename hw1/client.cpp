da34c3451de7ecf23af8d37261b6b798143dee81
Melhor <b05705046@ntu.edu.tw>
2020-12-12 15:49:31 +0800

    add check on host/client

--- hw1/client.cpp
+++ hw1/client.cpp
@@ -14,78 +14,98 @@ using namespace std;
 class Client
 {
 private:
-    int sock; // socket description
-    string address;
-    int server_port;
+    int server_sock; // socket description
+    int client_sock;
     struct sockaddr_in server;
+    struct sockaddr_in clientReciever;
 
 public:
     Client();
     bool isServerConnected;
-    bool connection(string, int); // create connection with server
-    bool send_data(string data);
-    void receive();
+    bool connection(bool withHost, string address, int port); // create connection with server
+    bool send_data(bool toHost, string data);
+    void receive(bool fromHost);
 };
 
 Client::Client()
 {
-    bzero(&server, sizeof(server)); // initialize
-    sock = -1;
-    server_port = 0;
-    address = "";
+    bzero(&server, sizeof(server));                 // initialize server
+    bzero(&clientReciever, sizeof(clientReciever)); // initialize clientReciever
+    server_sock = -1;
+    client_sock = -1;
     isServerConnected = false;
 }
 
-bool Client::connection(string address, int port)
+bool Client::connection(bool withHost, string address, int port)
 {
-    // create socket if it is not already created
-    if (sock == -1)
+    if (withHost)
     {
-        sock = socket(AF_INET, SOCK_STREAM, 0); // Create socket
-        if (sock == -1)
+        // create socket if it is not already created
+        if (server_sock == -1)
         {
-            perror("Could not create socket");
+            server_sock = socket(AF_INET, SOCK_STREAM, 0); // Create socket
+            if (server_sock == -1)
+            {
+                perror("Could not create socket");
+            }
+            cout << "Socket created\n";
         }
-        cout << "Socket created\n";
-    }
 
-    server.sin_addr.s_addr = inet_addr(address.c_str());
-    server.sin_family = AF_INET;
-    server.sin_port = htons(port);
-    
-    // Connect to remote server
-    if (connect(sock, (struct sockaddr *)&server, sizeof(server)) < 0)
+        server.sin_addr.s_addr = inet_addr(address.c_str());
+        server.sin_family = AF_INET;
+        server.sin_port = htons(port);
+
+        // Connect to remote server
+        if (connect(server_sock, (struct sockaddr *)&server, sizeof(server)) < 0)
+        {
+            perror("connect failed. Error");
+            return false;
+        }
+    }
+    else
     {
-        perror("connect failed. Error");
-        return false;
+        /* connect with client */
     }
 
     cout << "Connected\n";
     return true;
 }
 
-bool Client::send_data(string data)
+bool Client::send_data(bool toHost, string data)
 {
-    cout << "Sending data...";
-    cout << data << "\n";
+    cout << "Sending data to ";
 
-    if (send(sock, data.c_str(), strlen(data.c_str()), 0) < 0)
+    if (toHost)
     {
-        perror("Send failed : ");
-        return false;
+        cout << "host..." << data << "\n";
+        if (send(server_sock, data.c_str(), strlen(data.c_str()), 0) < 0)
+        {
+            perror("Send failed : ");
+            return false;
+        }
+    }
+    else
+    {
+        cout << "client..." << data << "\n";
     }
 
-    cout << "Data send\n";
     return true;
 }
 
-void Client::receive()
+void Client::receive(bool fromHost)
 {
     char buffer[2000] = {0};
-
     memset(buffer, '\0', sizeof(buffer));
-    int result = recv(sock, buffer, sizeof(buffer), 0);
-    cout << "response from server : \n" << buffer << "\n";
+
+    if (fromHost)
+    {
+        int result = recv(server_sock, buffer, sizeof(buffer), 0);
+        cout << "response from server : \n" << buffer << "\n";
+    }
+    else
+    {
+        /* Recieve from client */
+    }
 }
 
 int main(int argc, char *argv[])
@@ -98,21 +118,42 @@ int main(int argc, char *argv[])
     cin >> host >> server_port;
 
     pid_t pid = fork();
-    if (pid == -1) {
+    if (pid == -1)
+    {
         perror("fork fail");
-    } else if (pid == 0) {      // child id
+    }
+    else if (pid == 0)
+    { // child id, listen and print
         cout << "pid = 0, i am child \n";
-    } else {
+        /* TODO: listen to client port */
+    }
+    else
+    {
         cout << "pid " << pid << ", i am parent, connecting to server \n";
-        client.isServerConnected = client.connection(host, server_port); //connect to host
-        client.receive();
-        
-        while(client.isServerConnected)
+        client.isServerConnected = client.connection(true, host, server_port); //connect to host
+        client.receive(true);
+
+        while (client.isServerConnected)
         {
+            string receiver;
+            cout << "send command to host or client (h/c) ? ";
+            cin >> receiver;
+            bool withHost = receiver == "h";
+            if (!withHost)
+            {
+                string client_ip;
+                int client_port;
+                cout << "type in client receiver's ip and host: ";
+                cin >> client_ip >> client_port;
+
+                // connect with client
+            }
+
+            // send message
             string command;
             cin >> command;
-            client.send_data(command);
-            client.receive();
+            client.send_data(withHost, command);
+            client.receive(withHost);
         }
     }
 
