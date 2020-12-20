2c36aec4cd8e59899079c211b3304398e041982c
Melhor <b05705046@ntu.edu.tw>
2020-12-20 14:34:49 +0800

    add init threading

--- hw2/server.cpp
+++ hw2/server.cpp
@@ -10,28 +10,37 @@
 #include <sys/types.h>
 #include <stdlib.h>
 #include <netinet/in.h>
+#include <pthread.h>
 using namespace std;
+
+#define MAX_CLIENT 5
+typedef void * (*THREADFUNCPTR)(void *);
+
+int server_sock = -1;
 
 class Host
 {
 private:
-    int server_sock; // socket description
-    int client_sock;
+    pthread_t my_thread[MAX_CLIENT];
     struct sockaddr_in server;
+    void* client_thread(void* arg);
+    static void *client_thread_helper(void *context)
+    {
+        return ((Host *)context)->client_thread(context);
+    }
 
 public:
     Host();
     bool createSocket(int port); // create socket
     void listen_port();
-    bool send_data(string data);
-    void receive();
+    void Start();
+    bool send_data(int client_sock, string data);
+    void receive(int client_sock);
 };
 
 Host::Host()
 {
     bzero(&server, sizeof(server));
-    server_sock = -1;
-    client_sock = -1;
 }
 
 bool Host::createSocket(int port)
@@ -64,31 +73,10 @@ void Host::listen_port()
         exit(0);
     }
 
-    listen(server_sock, 5);
-
-    sockaddr_in newSocketAddr;
-    socklen_t newSocketSize = sizeof(newSocketAddr);
-
-    while (true)
-    {
-        client_sock = accept(server_sock, (sockaddr *)&newSocketAddr, &newSocketSize);
-        if (client_sock < 0)
-        {
-            cout << "client_sock fail: " << client_sock << endl;
-            cerr << "Can't accepting the request from client!" << endl;
-            exit(0);
-        }
-        cout << "Connection accepted from " << inet_ntoa(newSocketAddr.sin_addr) << " " << ntohs(newSocketAddr.sin_port) << endl;
-        send_data("Connection accepted!");
-
-        while(true)
-        {
-            receive();
-        }
-    }
+    listen(server_sock, MAX_CLIENT);
 }
 
-bool Host::send_data(string data)
+bool Host::send_data(int client_sock, string data)
 {
     cout << "Sending data..." << data << "\n";
     if (send(client_sock, data.c_str(), strlen(data.c_str()), 0) < 0)
@@ -100,7 +88,7 @@ bool Host::send_data(string data)
     return true;
 }
 
-void Host::receive()
+void Host::receive(int client_sock)
 {
     char buffer[2000] = {0};
     memset(buffer, '\0', sizeof(buffer));
@@ -108,8 +96,51 @@ void Host::receive()
     if (recv(client_sock, buffer, sizeof(buffer), 0) > 0)
     {
         cout << "client :" << buffer << "\n";
-        send_data(buffer);
+        send_data(client_sock, buffer);
     }
+}
+
+void* Host::client_thread(void* arg)
+{
+    int threadID = (int)(size_t)(arg);
+    cout << "This is worker_thread()" << threadID << "\n";
+    sockaddr_in newSocketAddr;
+    socklen_t newSocketSize = sizeof(newSocketAddr);
+
+    while (true)
+    {
+        int client_sock;
+        client_sock = accept(server_sock, (sockaddr *)&newSocketAddr, &newSocketSize);
+        if (client_sock < 0)
+        {
+            cout << "client_sock fail: " << client_sock << endl;
+            cerr << "Can't accepting the request from client!" << endl;
+            exit(0);
+        }
+        cout << "Connection accepted from " << inet_ntoa(newSocketAddr.sin_addr) << " " << ntohs(newSocketAddr.sin_port) << endl;
+        send_data(client_sock, "Connection accepted!");
+
+        while(true)
+        {
+            receive(client_sock);
+        }
+    }
+
+    pthread_exit(NULL);
+}
+
+void Host::Start()
+{
+    cout << "In main: creating thread\n";
+    for(int i = 0; i < MAX_CLIENT; i++) {
+        int ret =  pthread_create(&my_thread[i], NULL, &Host::client_thread_helper, (void*)i);
+        if(ret != 0) {
+            cout << "Error: pthread_create() failed\n";
+            exit(EXIT_FAILURE);
+        }
+    }
+
+    pthread_join(my_thread[0], NULL);
 }
 
 int main(int argc, char *argv[])
@@ -120,7 +151,9 @@ int main(int argc, char *argv[])
     cout << "type in your port: ";
     cin >> port;
     myserver.createSocket(port);
-    myserver.listen_port();
+    // myserver.listen_port();
+
+    myserver.Start();
 
     return 0;
 }
