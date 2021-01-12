f35b36d0ca93a6372d1f8c0f62037685893cdf16
Melhor <b05705046@ntu.edu.tw>
2021-01-12 15:11:46 +0800

    add client-side communication
    once

--- hw3/client_main.cpp
+++ hw3/client_main.cpp
@@ -27,9 +27,9 @@ int main(int argc, char *argv[])
         client.createSocket(false, local_addr, client_port);
         client.listen_port();
     }
-    else
+    else // connect to host
     {
-        client.createSocket(true, local_addr, server_port); //connect to host
+        client.createSocket(true, local_addr, server_port); 
         client.isServerConnected = client.connection(true);
 
         while (client.isServerConnected)
@@ -50,18 +50,48 @@ int main(int argc, char *argv[])
                 int port;
                 cout << "type in client receiver's port: ";
                 cin >> port;
-                cout << "client_sock = " << client.GetClientSock() << endl;
-                if (client.GetClientSock() == -1) {
-                    client.createSocket(withHost, local_addr, port);
-                    client.connection(withHost);
-                }
+                // client.createSocket(withHost, local_addr, port);
+                // client.connection(withHost);
                 
+                // string command;
+                // cout << "command: ";
+                // cin >> command;
+                // client.send_data(withHost, command);
+                // string response = client.receive(withHost);
+
+                int socket_transfer = 0;
+                struct sockaddr_in conn_addr;
+
+                socket_transfer = socket(AF_INET, SOCK_STREAM, 0);
+                if(socket_transfer < 0)
+                {
+                    cout << "--> Error in subjectively connectng socket.";
+                    continue;
+                }
+
+                bzero((char *)&conn_addr, sizeof(conn_addr));
+                conn_addr.sin_family = AF_INET;
+                conn_addr.sin_addr.s_addr = inet_addr(local_addr.c_str());
+                conn_addr.sin_port = htons(port);
+
+                // Connectng
+                if(connect(socket_transfer, (struct sockaddr*)&conn_addr, sizeof(conn_addr)) < 0)
+                {
+                    cout << "--> Error when connecting another client." << endl;
+                    continue;
+                }
+
                 string command;
                 cout << "command: ";
                 cin >> command;
-                client.send_data(withHost, command);
-                string response = client.receive(withHost);
 
+                if (send(socket_transfer, command.c_str(), strlen(command.c_str()), 0) < 0)
+                {
+                    perror("Send failed : ");
+                    return false;
+                }
+
+                close(socket_transfer);
             }
             else // talk to server
             {
